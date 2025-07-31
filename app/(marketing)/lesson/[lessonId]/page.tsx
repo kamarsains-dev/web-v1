8a594269a099c1f3e4b8bf65932049827aaf2b24
kamarsains-dev <kamarsainsdeveloper@gmail.com>
2025-08-01 01:58:14 +0700

    fix lesson V2

--- app/(marketing)/lesson/[lessonId]/page.tsx
+++ app/(marketing)/lesson/[lessonId]/page.tsx
@@ -2,14 +2,14 @@ import { getLesson, getUserProgress } from "@/lib/queries";
 import { Quiz } from "../../component/quiz";
 import { redirect } from "next/navigation";
 
-type Props = {
-    params: {
-        lessonId: number;
-    }
-}
+//type Props = {
+ //   params: {
+   //     lessonId: number;
+   // }
+//}
 
-const LessonIdPage = async ({params}:Props) => {
-    const activeLesson = await getLesson(params.lessonId); 
+const LessonIdPage = async () => {
+    const activeLesson = await getLesson(); 
     const userProgressData = await getUserProgress(); 
 
     const [lesson, userProgress] = await Promise.all([activeLesson, userProgressData])
